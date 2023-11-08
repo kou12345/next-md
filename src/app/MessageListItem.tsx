860c3d5afb5b04a3760eb0223ef80eedad08f886
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-09 00:52:45 +0900

    fix ui

--- src/app/MessageListItem.tsx
+++ src/app/MessageListItem.tsx
@@ -1,15 +1,30 @@
+import { useUser } from "@clerk/nextjs";
+import { Message } from "./Socket";
+
 type Props = {
-  message: string;
+  message: Message;
 };
 
 export const MessageListItem = (props: Props) => {
+  const { isSignedIn, user, isLoaded } = useUser();
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isSignedIn) {
+    return <div>Not signed in</div>;
+  }
+
+  const isOwnMessage = props.message.userId === user?.id;
+
   return (
-    <div className="chat chat-start">
+    <div className={`chat ${isOwnMessage ? "chat-end" : "chat-start"}`}>
       <div className="chat-header">
-        Obi-Wan Kenobi
+        {isOwnMessage ? user?.username : "Other User"}
         <time className="text-xs opacity-50">2 hours ago</time>
       </div>
-      <div className="chat-bubble">{props.message}</div>
+      <div className="chat-bubble">{props.message.message}</div>
     </div>
   );
 };
