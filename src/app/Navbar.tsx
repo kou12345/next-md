98d932c91e8ffb109030d2fc93650aaf94d91372
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-09 07:59:40 +0900

    fix

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a className="btn btn-ghost normal-case text-xl">Chat</a>
       </div>
       <div className="flex-none">
         <UserButton afterSignOutUrl="/" />
