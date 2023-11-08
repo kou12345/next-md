1505906e1afb3dad3c654c22b2afdf480b141f54
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-08 22:21:04 +0900

    fix ログイン中のユーザーを取得

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,15 @@
-"use client";
+"use server";
 
-import { useEffect, useRef, useState } from "react";
-import { MessageForm } from "./MessageForm";
-import { MessageList } from "./MessageList";
+import { currentUser } from "@clerk/nextjs";
+import { Socket } from "./Socket";
 
-export default function Home() {
-  const socketRef = useRef<WebSocket | null>(null);
-  const [messages, setMessages] = useState<string[]>([]);
-
-  useEffect(() => {
-    const existingSocket = socketRef.current;
-    if (existingSocket) {
-      existingSocket.close();
-    }
-
-    // 新しいWebSocketインスタンスを作成
-    const ws = new WebSocket("ws://localhost:8080");
-    socketRef.current = ws;
-
-    ws.onopen = () => {
-      console.log("Connected to the WebSocket server");
-    };
-
-    ws.onmessage = (event) => {
-      console.log("Message from server:", event.data);
-      setMessages((prevMessages) => [...prevMessages, event.data]);
-    };
-
-    ws.onclose = () => {
-      console.log("Disconnected from the WebSocket server");
-    };
-
-    ws.onerror = (error) => {
-      console.error("WebSocket error:", error);
-    };
-
-    // コンポーネントのアンマウント時にWebSocket接続を閉じる
-    return () => {
-      ws.close();
-    };
-  }, []);
-
-  // メッセージをサーバーに送信する関数
-  const sendMessage = (message: string) => {
-    if (socketRef.current) {
-      socketRef.current.send(message);
-    } else {
-      console.error("WebSocket instance is not ready.");
-    }
-  };
+export default async function Home() {
+  const user = await currentUser();
+  console.log(user?.id);
 
   return (
     <div>
-      <h1>WebSocket Test</h1>
-      <button onClick={() => sendMessage("Hello Server!")}>Send Message</button>
-      <div>
-        <h2>Messages:</h2>
-        <MessageList messages={messages} />
-      </div>
-      <MessageForm sendMessage={sendMessage} />
+      <Socket userId={user?.id as string} />
     </div>
   );
 }
