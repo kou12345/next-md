164079e375f9513cb2e18b2524cea4c267c31b4e
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-09 01:21:03 +0900

    ws serverの urlをenvに変更

--- src/app/Socket.tsx
+++ src/app/Socket.tsx
@@ -25,7 +25,7 @@ export const Socket = (props: Props) => {
     }
 
     // 新しいWebSocketインスタンスを作成
-    const ws = new WebSocket("ws://localhost:8080");
+    const ws = new WebSocket(process.env.NEXT_PUBLIC_WS_URL as string);
     socketRef.current = ws;
 
     ws.onopen = () => {
@@ -66,12 +66,13 @@ export const Socket = (props: Props) => {
   };
 
   return (
-    <div>
-      <div>
-        <h2>Messages:</h2>
+    <div className="w-4/5 flex justify-center items-center">
+      <div className="justify-center">
         <MessageList messages={messages} />
+        <div>
+          <MessageForm sendMessage={sendMessage} />
+        </div>
       </div>
-      <MessageForm sendMessage={sendMessage} />
     </div>
   );
 };
