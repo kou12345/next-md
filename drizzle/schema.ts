cb6483a4995060fcc57c3df5b9c198bf024e6a29
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-12 23:15:44 +0900

    fix schema

---
+++ drizzle/schema.ts
@@ -0,0 +1,27 @@
+import { mysqlTable, mysqlSchema, AnyMySqlColumn, primaryKey, int, text, datetime, varchar } from "drizzle-orm/mysql-core"
+import { sql } from "drizzle-orm"
+
+
+export const messages = mysqlTable("messages", {
+	id: int("id").autoincrement().notNull(),
+	roomId: int("room_id").notNull(),
+	userId: text("user_id").notNull(),
+	messageText: text("message_text").notNull(),
+	createdAt: datetime("created_at", { mode: 'string'}).default(sql`CURRENT_TIMESTAMP`),
+},
+(table) => {
+	return {
+		messagesId: primaryKey(table.id),
+	}
+});
+
+export const rooms = mysqlTable("rooms", {
+	id: int("id").autoincrement().notNull(),
+	roomName: varchar("room_name", { length: 255 }).notNull(),
+	createdAt: datetime("created_at", { mode: 'string'}).default(sql`CURRENT_TIMESTAMP`),
+},
+(table) => {
+	return {
+		roomsId: primaryKey(table.id),
+	}
+});
