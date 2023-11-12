a6f39ebb8ea3527e4f1fed35be9e4c352c3eeaab
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-12 23:17:48 +0900

    add チャットルーム一覧を表示するcomponent

---
+++ src/app/ChatRoomList.tsx
@@ -0,0 +1,19 @@
+import { getChatRoomList } from "@/server/chatRooms.ts/chatRoomList";
+import Link from "next/link";
+
+export const ChatRoomList = async () => {
+  const ChatRoomList = await getChatRoomList();
+  console.log(ChatRoomList);
+  return (
+    <div>
+      <div>チャットルーム一覧</div>
+      <ul>
+        {ChatRoomList?.map((chatRoom) => (
+          <li key={chatRoom.id}>
+            <Link href={`/chatRoom/${chatRoom.id}`}>{chatRoom.roomName}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
