9b3f1ad3cb6334248ab1dee7932cd5126d5959ed
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-13 00:56:05 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 "use server";
 
-import { currentUser } from "@clerk/nextjs";
-import { Socket } from "./Socket";
+import { ChatRoomList } from "./ChatRoomList";
 
 export default async function Home() {
-  const user = await currentUser();
-
   return (
     <div className="w-full flex justify-center items-center">
-      <Socket userId={user?.id as string} userName={user?.username as string} />
+      <ChatRoomList />
     </div>
   );
 }
