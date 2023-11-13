d8cf45bea95ed53dab9f2e70093b7fd4ff163b81
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-13 10:42:08 +0900

    test

--- src/hooks/useChatHistory.ts
+++ src/hooks/useChatHistory.ts
@@ -1,5 +1,4 @@
 import { getChatHistory } from "@/server/chatHistory";
-import { getUserName } from "@/server/user";
 import { Message } from "@/types/types";
 import { useEffect, useState } from "react";
 
@@ -12,11 +11,13 @@ export const useChatHistory = (roomId: number) => {
       if (result) {
         const messagesWithUser = await Promise.all(
           result.map(async (message) => {
-            const userName = await getUserName(message.userId);
+            // ! ここのせいで遅くなっている説
+            // const userName = await getUserName(message.userId);
             return {
               message: message.messageText as string,
               userId: message.userId as string,
-              userName: userName as string,
+              // userName: userName as string,
+              userName: "test",
             };
           })
         );
